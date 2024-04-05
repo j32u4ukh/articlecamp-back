30313ecbb2fd0998dc2ff6c9633d2c3d24abf4d1
j32u4ukh <j32u4ukh@gmail.com>
2024-04-05 16:46:47 +0800

    feat: 文章分類串接資料庫

--- services/categories.js
+++ services/categories.js
@@ -1,23 +1,24 @@
+const db = require('../models')
+const { ErrorCode } = require('../utils/codes')
+const Category = db.category
 
-const db = require("../models");
-const { ErrorCode } = require('../utils/codes');
-const Category = db.category;
-
-class CategoryService {  
+class CategoryService {
   getList() {
     return new Promise((resolve, reject) => {
       Category.findAll({
         attributes: ['id', 'name'],
         raw: true,
-      }).then((categories) => {
-        resolve(categories)
-      }).catch((error)=>{
-        console.log(`讀取文章分類數據時發生錯誤, error: ${error}`)
-        reject({
-          code: ErrorCode.ReadError,
-          msg: "讀取文章分類數據時發生錯誤",
-        })
       })
+        .then((categories) => {
+          resolve(categories)
+        })
+        .catch((error) => {
+          console.log(`讀取文章分類數據時發生錯誤, error: ${error}`)
+          reject({
+            code: ErrorCode.ReadError,
+            msg: '讀取文章分類數據時發生錯誤',
+          })
+        })
     })
   }
 }
