5d1d2ccb29d3d813b814e59e737012cf7ba36ed6
j32u4ukh <j32u4ukh@gmail.com>
2024-04-07 18:38:12 +0800

    feat: 移除本地資料用的 models

--- _models/index.js
+++
@@ -1,7 +0,0 @@
-const Article = require('./articles')
-const Category = require('./categories')
-const Message = require('./messages')
-const User = require('./users')
-const Follow = require('./follows')
-
-module.exports = { Article, Category, Message, User, Follow }
