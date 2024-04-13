1d1f12d3a410e1d89858df1875405641de9c5652
j32u4ukh <j32u4ukh@gmail.com>
2024-04-13 08:46:41 +0800

    feat: 修正文章 seed 當中的文章分類

--- seeders/20240404073802-init-article.js
+++ seeders/20240404073802-init-article.js
@@ -1,50 +1,50 @@
-'use strict';
+'use strict'
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    const transaction = await queryInterface.sequelize.transaction();
-    try{
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
       // articles
       await queryInterface.bulkInsert(
-        "articles",
+        'articles',
         Array.from({ length: 50 }).map((_, i) => {
           return {
-            id: i+1,
-            userId: i%5+1,
+            id: i + 1,
+            userId: (i % 5) + 1,
             title: `Article-${i}`,
-            category: i%4,
+            category: (i % 4) + 1,
             content: `This is content ${i}`,
             createdAt: new Date(),
             updatedAt: new Date(),
-          };
+          }
         })
-      );   
-      // messages   
+      )
+      // messages
       await queryInterface.bulkInsert(
-        "messages",
+        'messages',
         Array.from({ length: 50 }).map((_, i) => {
           return {
-            id: i+1,
-            userId: i%5+1,
-            articleId: i%10,
+            id: i + 1,
+            userId: (i % 5) + 1,
+            articleId: i % 10,
             content: `This is message ${i}`,
             createdAt: new Date(),
             updatedAt: new Date(),
-          };
+          }
         }),
         {
           transaction,
         }
-      );
-      await transaction.commit();
+      )
+      await transaction.commit()
     } catch (error) {
-      console.log(`執行 seeder 失敗, error: ${error}`);
-      await transaction.rollback();
+      console.log(`執行 seeder 失敗, error: ${error}`)
+      await transaction.rollback()
     }
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("articles", null);
-  }
-};
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('articles', null)
+  },
+}
